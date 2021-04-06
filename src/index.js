f1f06c6ad594dc7f20f0e21f54fd37d3b4142389
Pitt Wu <kgb00128@gmail.com>
2021-04-06 15:43:48 +0800

    fixed：調整 env key 名稱

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = `${process.env.REACT_APP_AUTH0_DOMIN}`;
+const domain = `${process.env.REACT_APP_AUTH0_DOMAIN}`;
 const clientId = `${process.env.REACT_APP_AUTH0_KEY}`;
 
 ReactDOM.render(
